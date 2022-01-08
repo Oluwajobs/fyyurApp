b212bd6d2858f12c9793ad2ed54f014a2c6e4a19
Ibrahim Oluwajoba Adisa <oluwajobawilliams@gmail.com>
2022-01-07 22:22:41 -0500

    Update modules import

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 #----------------------------------------------------------------------------#
 # Imports
 #----------------------------------------------------------------------------#
-
+import sys
 import json
 import dateutil.parser
 import babel
 from flask import Flask, render_template, request, Response, flash, redirect, url_for
+from sqlalchemy import func
 from flask_moment import Moment
-from flask_sqlalchemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy 
 import logging
+from flask_migrate import Migrate
 from logging import Formatter, FileHandler
 from flask_wtf import Form
 from forms import *

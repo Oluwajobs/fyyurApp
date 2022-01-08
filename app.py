8c0c400e8209c60f16342af3a4700be4d9069f14
Ibrahim Oluwajoba Adisa <oluwajobawilliams@gmail.com>
2022-01-07 22:45:53 -0500

    Create initial models for migration

--- app.py
+++ app.py
@@ -34,31 +34,49 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String())
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     address = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
+
+    
+    def __repr__(self):
+      return f"<Venue {self.name}>"
+
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
+    name = db.Column(db.String())
     city = db.Column(db.String(120))
     state = db.Column(db.String(120))
     phone = db.Column(db.String(120))
     genres = db.Column(db.String(120))
     image_link = db.Column(db.String(500))
     facebook_link = db.Column(db.String(120))
-
     # TODO: implement any missing fields, as a database migration using Flask-Migrate
 
+    def __repr__(self):
+      return f"<Artist {self.name}>"
+
+
+
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
+class Show(db.Model):
+  __tablename__ = 'Show'
+
+  id = db.Column(db.Integer, primary_key=True)
+  artist_id = db.Column(db.Integer, db.ForeignKey("Artist.id"))
+  venue_id = db.Column(db.Integer, db.ForeignKey("Venue.id"))
+  start_time = db.Column(db.DateTime)
+
+  def __repr__(self):
+    return f"<Show {self.artist_id} {self.venue_id}>"
 
 #----------------------------------------------------------------------------#
 # Filters.
